3567dfb0ee5300b1a1a12db121104be214dbe1dd
Christian Vinícius <christian.santos@sou.inteli.edu.br>
2025-02-04 20:26:07 -0300

    Mudanças finais

--- peixes.js
+++ peixes.js
@@ -13,17 +13,20 @@ var config = {
 var game = new Phaser.Game(config);
 
 var peixinho;
+var bolha;
 
 function preload() {
     this.load.image('mar', 'assets/bg_azul-claro.png');
     this.load.image('logo', 'assets/logo-inteli_azul.png');
     this.load.image('peixe', 'assets/peixes/baiacu.png');
+    this.load.image('bolha', 'assets/bolhas.png')
 }
 
 function create() {
     this.add.image(400, 300, 'mar');
     this.add.image(400, 525, 'logo').setScale(0.5);
     peixinho = this.add.image(400, 300, 'peixe').setOrigin(0.5, 0.5).setFlip(true, false);
+    bolha = this.add.image (200, 500, 'bolha');
 
     peixinho.setFlip(true, false);
 }
